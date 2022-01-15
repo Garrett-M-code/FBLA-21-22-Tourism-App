54a02d9e320fc0a902f17c6bda6de3b034debb25
Garrett-M-code <84481668+Garrett-M-code@users.noreply.github.com>
2022-01-14 19:18:11 -0500

    Added to Main
    Added comments and added code that runs the GUI.java file.

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-public class Main {
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends GUI{
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    // This code is run automatically
+    // These are non-functioning arrays
+    String[] attractionList = {"------", "Hotel", "Entertainment", "Resturant", "Park", "Shop"};
+    String[] ratingList = {"------", "5 Stars", "4 Stars", "3 Stars", "2 Stars", "1 Star"};
+    String[] proximityList = {"------", "1 Mile Away", "5 Miles Away", "10 Miles Away", "15 Miles Away"};
+    String[] priceList = {"------", "Free", "$", "$$", "$$$"};
+    String[] workHoursList = {"------", "Morning", "Afternoon", "Night"};
+
+    // This creates a GUI using the GUI.java file.
+    GUI instance = new GUI();
+    // These are the parameters of the application when ran.
+    instance.setSize(new Dimension(1000,600));
+    instance.setVisible(true);
   }
 }
