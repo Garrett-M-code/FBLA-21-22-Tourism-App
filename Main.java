22646940e44a0e72f5efecffe047b87fc062e196
Garrett-M-code <84481668+Garrett-M-code@users.noreply.github.com>
2022-01-15 22:29:23 -0500

    Not Much
    Simply compressed the code and added a new method.

--- Main.java
+++ Main.java
@@ -6,18 +6,17 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main extends GUI{
-  public static void main(String[] args) {
-    // This code is run automatically
-    // These are non-functioning arrays
-    String[] attractionList = {"------", "Hotel", "Entertainment", "Resturant", "Park", "Shop"};
-    String[] ratingList = {"------", "5 Stars", "4 Stars", "3 Stars", "2 Stars", "1 Star"};
-    String[] proximityList = {"------", "1 Mile Away", "5 Miles Away", "10 Miles Away", "15 Miles Away"};
-    String[] priceList = {"------", "Free", "$", "$$", "$$$"};
-    String[] workHoursList = {"------", "Morning", "Afternoon", "Night"};
+public class Main extends GUI {
+  public static void reader() {
+    // This will deal with the deployment of the Reader.java file.
+    System.out.println("Hello World");
+  }
 
+  public static void main(String[] args) {
+    // This code will run automatically
     // This creates a GUI using the GUI.java file.
     GUI instance = new GUI();
+    
     // These are the parameters of the application when ran.
     instance.setSize(new Dimension(1000,600));
     instance.setVisible(true);
