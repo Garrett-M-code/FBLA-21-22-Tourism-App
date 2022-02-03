4c2b27cea66e57b49327971560ac0fdb395c7676
Garrett-M-code <84481668+Garrett-M-code@users.noreply.github.com>
2022-02-03 02:07:25 -0500

    Bug Fixes and Final touches

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
+// This imports JFram and the rest of the Swing Library.
 import javax.swing.*;
 
+// This imports the libraries used for opening files and possible errors.
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+// This imports the libraries required for using JSON.
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+// This imports some Awt libraries that are used with Swing.
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
+// This imports utilities needed like Hash Maps.
 import java.util.Iterator;
 import java.util.Map;
 
 
 public class Main extends GUI {
-  public static void reader() {
-    // This will deal with the deployment of the Reader.java file.
-    System.out.println("Hello World");
-  }
-
   public static void main(String[] args) throws FileNotFoundException {
     // This code will run automatically
     // This creates a GUI using the GUI.java file.
